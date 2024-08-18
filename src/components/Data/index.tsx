9f989f3a527b595b511603615904da7940ec8eb4
SdZika <markozivkovic1982@gmail.com>
2024-08-18 17:00:06 +0200

    add data file for myWorks

---
+++ src/components/Data/index.tsx
@@ -0,0 +1,24 @@
+import proj1 from "../../assets/proj1.png";
+import proj2 from "../../assets/proj2.png";
+import proj3 from "../../assets/proj3.png";
+
+export const myWorks = [
+  {
+    id: 0,
+    title: "Score Board",
+    url: "https://sdzika.github.io/react-score-board/",
+    image: proj1,
+  },
+  {
+    id: 1,
+    title: "Fitnes Tracker",
+    url: "https://sdzika.github.io/workout-logging-form/",
+    image: proj2,
+  },
+  {
+    id: 2,
+    title: "Fitnes Tracker",
+    url: "https://mndraca.github.io/BookReview-/",
+    image: proj3,
+  },
+];
--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-import proj1 from "../assets/proj1.png";
-import proj2 from "../assets/proj2.png";
-import proj3 from "../assets/proj3.png";
+import { myWorks } from "./Data";
 
 export const Work = () => {
   return (
@@ -12,65 +10,26 @@ export const Work = () => {
         <p className="text-gray-400 mx-6">Check out some of my recent work</p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mx-6">
-        <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center bg-cover relative">
-          <img src={proj1} alt="proj1 " />
-          <div className="opacity-0 group-hover:opacity-90 bg-[grey]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider">
-              Score Board
-            </span>
-            <div className="pt-8 text-center">
-              <a
-                href="https://sdzika.github.io/react-score-board/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <button className="text-center rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg">
-                  Live
-                </button>
-              </a>
+        {myWorks.map((mywork, index) => (
+          <div
+            className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center bg-cover relative"
+            key={mywork.id}
+          >
+            <img src={mywork.image} alt={mywork.title} />
+            <div className="opacity-0 group-hover:opacity-90 bg-[grey]/70 absolute inset-0 flex flex-col justify-center items-center">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                {mywork.title}
+              </span>
+              <div className="pt-8 text-center">
+                <a href={mywork.url} target="_blank" rel="noreferrer">
+                  <button className="text-center rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg">
+                    Live
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center bg-cover relative">
-          <img src={proj2} alt="proj1" />
-          <div className="opacity-0 group-hover:opacity-90 bg-[grey]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider">
-              Fitnes Tracker
-            </span>
-            <div className="pt-8 text-center">
-              <a
-                href="https://sdzika.github.io/workout-logging-form/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <button className="text-center rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg">
-                  Live
-                </button>
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group rounded-md flex justify-center items-center bg-cover relative ">
-          <img src={proj3} alt="proj1" />
-          <div className="opacity-0 group-hover:opacity-90 bg-[grey]/70 absolute inset-0 flex flex-col justify-center items-center">
-            <span className="text-2xl font-bold text-white tracking-wider">
-              Book Review App
-            </span>
-            <div className="pt-8 text-center">
-              <a
-                href="https://mndraca.github.io/BookReview-/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <button className="text-center rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg">
-                  Live
-                </button>
-              </a>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
